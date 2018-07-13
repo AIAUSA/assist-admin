8f77b3c1ef2edd3e338e48d931a5093ccbb3cc86
SBB2 <sbb2@SBB2s-MacBook-Air.local>
2018-07-13 16:35:44 -0400

    nav still needs work

--- src/app/twentyday-dash/twentyday-dash.component.ts
+++ src/app/twentyday-dash/twentyday-dash.component.ts
@@ -18,7 +18,7 @@ export class TwentydayDashComponent implements OnInit {
 
   getUsers(): void {
     this.userService.getUsers()
-      .subscribe(users => this.users = users.slice(1, 5));
+      .subscribe(users => this.users = users.slice(0, 5));
   }
 
   getColor(id) {
